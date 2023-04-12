c78df150d4a776603c3eee826975717b7ba068c6
Vos87 <112941106+Vos87@users.noreply.github.com>
2023-04-12 20:12:10 +0200

    done

--- rsvp.php
+++ rsvp.php
@@ -1,13 +1,15 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $mail_to = "<INSERT_EMAIL_HERE>";
+        $mail_to = "talbot.batty19@gmail.com";
         
         # Sender Data
         $guest = trim($_POST["guest"]);
         $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $song = trim($_POST["song"]);
-        $message = trim($_POST["message"]);
+        $partner = trim($_POST["partner"]);
+        $children = trim($_POST["children"]);
+		$song = trim($_POST["song"]);
+		$diet = trim($_POST["diet"]);
         
         if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             http_response_code(400);
@@ -18,9 +20,11 @@
         # Mail Content
         $content = "Name: $name\n";
         $content .= "Email: $email\n";
-        $content .= "Song: $song\n";
+        $content .= "partner: $partner\n";
         $content .= "Guest: $guest\n";
-        $content .= "Message:\n$message\n";
+        $content .= "Children:\n$children\n";
+		$content .= "Song:\n$song\n";
+		$content .= "Diet:\n$diet\n";
 
         # email headers.
         $headers = "From: $name &lt;$email&gt;";
